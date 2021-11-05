f64bf0b9af319f26e12026d50371dbf9a6fec358
Lucas Giura <lucasgiura@gmail.com>
2021-11-05 19:16:25 -0300

    Exercicios

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+const nota = 30
+
+if (nota > 80){
+    console.log('APROVADO')
+} else if (nota < 80 && nota > 60) {
+    console.log('ESTÁ NA FILA DE ESPERA')
+} else {
+    console.log('REPROVADO')
+}
