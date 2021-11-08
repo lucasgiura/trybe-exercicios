40e4756b86c9bbce41607c52dd6fda59509e49d6
Lucas Giura <lucasgiura@gmail.com>
2021-11-08 15:48:52 -0300

    Exercicios de fixaçao

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/Exercicios-de-fixaçao.js
@@ -0,0 +1,41 @@
+/* Exercicio de fixação - 1 */
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);*/
+
+/* Exercício de fixação - 2 
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[menu.length - 3];
+
+console.log(indexOfPortfolio);*/
+
+/* Exercício de fixação - 3 */
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("Contato")
+console.log(menu);
+
+/* For - Exercicio de fixação */
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
+/* For/of - Exercicio de fixação */
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(name)
+    
+}
+
+let namee = 'João';
+
+for (let letras of namee) {
+    console.log(letras)
+    
+}
