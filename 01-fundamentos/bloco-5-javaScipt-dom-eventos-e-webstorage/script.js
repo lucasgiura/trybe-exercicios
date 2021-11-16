ed74ea93512d2c1f7862cf35d8b83d77001caeb9
Lucas Giura <lucasgiura@gmail.com>
2021-11-16 19:30:40 -0300

    Exercicios de fixaçao do bloco 5 - 1

---
+++ 01-fundamentos/bloco-5-javaScipt-dom-eventos-e-webstorage/script.js
@@ -0,0 +1,16 @@
+document.querySelector('#header-container').style.background = 'green';
+document.querySelector('.emergency-tasks').style.background = 'pink';
+
+document.querySelectorAll('.emergency-tasks h3')
+let bground = document.querySelectorAll('.emergency-tasks h3')
+for(i=0 ; i< bground.length; i+=1){
+    bground[i].style.background = 'purple'
+};
+
+document.querySelector('.no-emergency-tasks').style.background = 'yellow';
+
+let bground2 = document.querySelectorAll('.no-emergency-tasks h3')
+for(i=0 ; i< bground.length; i+=1){
+    bground2[i].style.background = 'black'
+}
+
