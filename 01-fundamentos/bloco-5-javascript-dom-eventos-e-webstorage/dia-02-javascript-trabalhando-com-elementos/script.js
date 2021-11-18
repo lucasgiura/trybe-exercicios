e3ab3627541e4ac48e35e17f4b4c29f1bd6a1ee0
Lucas Giura <lucasgiura@gmail.com>
2021-11-18 13:22:25 -0300

    Exercicios fixacao

---
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,27 @@
+/* document.querySelector('#primeiroFilhoDoFilho').innerText = 'Testando'
+console.log(document.querySelector('#pai').lastElementChild.previousElementSibling) */
+
+/* let irmaoParaElemento = '5 irmao';
+
+let ondeEsta = document.querySelector('#pai');
+    let resposta = document.createElement('section');
+    resposta.id = 'QuintoFilho'
+resposta.innerText = irmaoParaElemento
+
+ondeEsta.appendChild(resposta)
+
+resposta.removeChild() */
+
+//
+/* let filho = 'newChild'
+let pai = document.querySelector('#elementoOndeVoceEsta');
+
+let newBaby = document.createElement('section')
+    newBaby.innerText = filho
+
+ pai.appendChild(newBaby)
+
+
+ */
+ 
+
